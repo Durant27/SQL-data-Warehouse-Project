82e4307bb380e41aa1a38a8c5b23d533506c25c9
DJ <77516918+MasteringTechwithDJ@users.noreply.github.com>
2026-05-14 09:45:14 -0500

    Refactor material status and gender fields in SQL

--- scripts/Silver/Proc_load_silver.sql
+++ scripts/Silver/Proc_load_silver.sql
@@ -3,8 +3,14 @@ cst_id,
 cst_key,
 TRIM(cst_firstname) AS cst_firstname,
 TRIM(cst_lastname) AS cst_lastname,
-cst_material_status,
-cst_gndr,
+CASE WHEN UPPER (TRIM(cst_material_status)) = 'S' THEN 'Single'
+	 WHEN UPPER (TRIM(cst_material_status)) = 'M' THEN 'Married'
+	 ELSE 'n/a'
+END cst_material_status,
+CASE WHEN UPPER (TRIM(cst_gndr)) = 'F' THEN 'FEMALE'
+	 WHEN UPPER (TRIM(cst_gndr)) = 'm' THEN 'MALE'
+	 ELSE 'n/a'
+END cst_gndr,
 cst_create_date
 FROM(
 	SELECT
